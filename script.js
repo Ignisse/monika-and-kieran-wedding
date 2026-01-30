f1b91bde3a79d0df7aa176c17d732bda4b0109cd
Ignisse <moncathl@gmail.com>
2026-01-30 17:47:51 +0100

    netlify subject change

--- script.js
+++ script.js
@@ -84,6 +84,12 @@ if (form) {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
+const nameField = document.getElementById("full-name");
+        const subjectField = document.getElementById("rsvp-subject");
+        if (nameField && subjectField) {
+            subjectField.value = nameField.value; 
+        }
+
         const formData = new FormData(form);
 
         fetch("/", {
